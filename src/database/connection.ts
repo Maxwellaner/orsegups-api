d7338dd060ae7a0676107f033ce65d0035754f1a
Maxwell Laner <maxwellaner@gmail.com>
2021-12-15 20:08:19 -0300

    Change the connection to save the database in another file in case of tests

--- src/database/connection.ts
+++ src/database/connection.ts
@@ -1,8 +1,10 @@
 import { Sequelize } from 'sequelize';
 
+const path = process.env.NODE_ENV == "test" ? './test.sqlite' : process.env.DB_PATH;
+
 const sequelize = new Sequelize({
   dialect: 'sqlite',
-  storage: './db.sqlite'
+  storage: `${path}`
 });
 
 export default sequelize;
