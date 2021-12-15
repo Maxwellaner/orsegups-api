b539fe171919d529289f76cd7983cb2b6fca4bd6
Maxwell Laner <maxwellaner@gmail.com>
2021-12-15 20:07:15 -0300

    Change the main to only start the app by passing the port

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,4 @@
-import App from './app';
-import ContactController from './modules/contact/controllers/contact.controller';
-import sequelize from './database/connection';
+import app from './app';
+import 'dotenv/config';
 
-sequelize.sync();
-const app = new App([new ContactController()], 4000);
-
-app.listen();
+app.listen(process.env.PORT || 4000);
