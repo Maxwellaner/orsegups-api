5b34d4c430c77e9549e0ffc1351438924d64316b
Maxwell Laner <maxwellaner@gmail.com>
2021-12-16 11:20:55 -0300

    Add put and delete methods and fixed some logical

--- src/modules/contact/services/contact.service.ts
+++ src/modules/contact/services/contact.service.ts
@@ -1,18 +1,22 @@
 import IContactRepository from "../interface/IContactRepository";
 import Contact from "../../../sequelize-models/contact.model";
+import { PutDto } from "../dto/create.dto";
 
 export default class ContactService {
   constructor(private readonly repository: IContactRepository) {}
 
   public async create(contact: Contact): Promise<Contact> {
-    if (await this.repository.checkIfContactExists(contact)) throw new Error('Contact already exists');
+    const exists = await this.getByEmail(contact.email);
+    if (exists) throw new Error('Contact already exists');
     const response = await this.repository.create(contact);
     contact.id = response.id;
     return contact;
   }
 
   public async getById(id: number): Promise<Contact | null | undefined> {
-    return await this.repository.findByPk(id);
+    const contact = await this.repository.findByPk(id);
+    if (!contact) throw new Error('Contact dont exists');
+    return contact;
   }
 
   public async getAll(): Promise<Contact[]> {
@@ -21,5 +25,17 @@ export default class ContactService {
 
   public async getByEmail(email: string): Promise<Contact | null | undefined> {
     return await this.repository.findOne(email);
+  }
+
+  public async put(id: number, dto: PutDto): Promise<Contact> {
+    if (!id) throw new Error('Invalid contact identifier');
+    await this.getById(id);
+    return await this.repository.put(id, dto);
+  }
+
+  public async delete(id: number): Promise<void> {
+    if (!id) throw new Error('Invalid contact identifier');
+    await this.getById(id);
+    return await this.repository.delete(id);
   }
 }
