144e8ba8c9cce5be2b6564ffd132c674e3e7537b
Maxwell Laner <maxwellaner@gmail.com>
2021-12-16 11:21:42 -0300

    Fixed contact informations and remove unused test

--- src/tests/modules/contact/integration/create.spec.ts
+++ src/tests/modules/contact/integration/create.spec.ts
@@ -4,17 +4,13 @@ import request from 'supertest';
 describe("Create contacts", () => {
   it("should be able to create a new contact", async () => {
     const response = await request(app).post("/api/contacts/create").send({
-      name: 'kkk',
+      name: 'Integration create',
       contactType: 'familiar',
       phone: "53991039390",
-      email: "kkk@gmail.com"
+      email: "integration-create@gmail.com"
     });
     
     expect(response.status).toBe(201);
     expect(response.body.contact).toHaveProperty("id");
-  })
-  it("shoud", async () => {
-    const response = await request(app).get("/api/contacts");
-    expect(response.status).toBe(200);
   })
 })
