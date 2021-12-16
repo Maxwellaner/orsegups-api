2222bb8be9a1a31ba3df02cfc7c0323d3abbffea
Maxwell Laner <maxwellaner@gmail.com>
2021-12-16 11:20:14 -0300

    Add put and delete methods, add dtos and fixed nomenclatures

--- src/modules/contact/repository/contactRepository.ts
+++ src/modules/contact/repository/contactRepository.ts
@@ -1,25 +1,36 @@
 import IContactRepository from "../interface/IContactRepository";
-import contactModel from "../../../sequelize-models/contact.model";
+import Contact from "../../../sequelize-models/contact.model";
 
 import SequelizeContact from "../models/sequelize.contact";
+import { CreateDto, PutDto } from "../dto/create.dto";
 
 export default class ContactRepository implements IContactRepository {
-  async create(contact: contactModel): Promise<contactModel> {
+  async create(contact: CreateDto): Promise<Contact> {
     return await (await SequelizeContact.create(contact)).toJSON();
   }
-  async findByPk(id: number): Promise<contactModel | null | undefined> {
+  async findByPk(id: number): Promise<Contact | null | undefined> {
     return (await SequelizeContact.findByPk(id))?.toJSON();
   }
-  async findOne(email: string): Promise<contactModel | null | undefined> {
+  async findOne(email: string): Promise<Contact | null | undefined> {
     return (await SequelizeContact.findOne({ where: { email } }))?.toJSON();
   }
-  async findAll(): Promise<contactModel[]> {
-    const contacts: contactModel[] = [];
+  async findAll(): Promise<Contact[]> {
+    const contacts: Contact[] = [];
     const response = await SequelizeContact.findAll();
     response.forEach(contact => contacts.push(contact.toJSON()));
     return contacts;
   }
-  async checkIfContactExists(contact: contactModel): Promise<Boolean> {
+  async checkIfContactExists(contact: Contact): Promise<Boolean> {
     return !!await this.findOne(contact.email);
+  }
+  async put(id: number, dto: PutDto): Promise<Contact> {
+    const contact = await SequelizeContact.findByPk(id) as SequelizeContact;
+    contact.set(dto);
+    await contact.save();
+    return contact.toJSON();
+  }
+  async delete(id: number): Promise<void> {
+    const contact = await SequelizeContact.findByPk(id) as SequelizeContact;
+    return await contact.destroy();
   }
 }
