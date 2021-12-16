74dc45aa9aa1a49ff758dc8913a43f4da6459ee6
Maxwell Laner <maxwellaner@gmail.com>
2021-12-16 11:18:44 -0300

    Add put and delete methods and fixed some logics

--- src/modules/contact/interface/IContactRepository.ts
+++ src/modules/contact/interface/IContactRepository.ts
@@ -1,9 +1,11 @@
 import Contact from "../../../sequelize-models/contact.model";
+import { CreateDto, PutDto } from "../dto/create.dto";
 
 export default interface IContactRepository {
-  create(contact: Contact): Promise<Contact>;
+  create(contact: CreateDto): Promise<Contact>;
   findByPk(id: number): Promise<Contact | null | undefined>;
   findOne(email: string): Promise<Contact | null | undefined>;
   findAll(): Promise<Contact[]>;
-  checkIfContactExists(contact: Contact): Promise<Boolean>;
+  put(id: number, dto: PutDto): Promise<Contact>;
+  delete(id: number): Promise<void>;
 }
